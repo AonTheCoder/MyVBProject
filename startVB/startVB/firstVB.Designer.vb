21a6d2878f7b41111247b15c4a905fb947a7abc0
aon_coder <aon.craze58@gmail.com>
2018-12-03 11:47:39 +0700

    upload3ธค61

---
+++ startVB/startVB/firstVB.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class firstVB
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txthour = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtpay = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.txtresult = New System.Windows.Forms.TextBox()
+        Me.btcal = New System.Windows.Forms.Button()
+        Me.btclose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(192, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(182, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "โปรเเกรมคำนวณภาษี"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.Location = New System.Drawing.Point(78, 142)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "อัตราค่าจ้าง"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(78, 103)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "เงินเดือน"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label4.Location = New System.Drawing.Point(78, 178)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "เป็นเงิน"
+        '
+        'txthour
+        '
+        Me.txthour.Location = New System.Drawing.Point(232, 105)
+        Me.txthour.Name = "txthour"
+        Me.txthour.Size = New System.Drawing.Size(100, 20)
+        Me.txthour.TabIndex = 0
+        Me.txthour.TabStop = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(232, 144)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(232, 180)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'txtpay
+        '
+        Me.txtpay.Location = New System.Drawing.Point(232, 144)
+        Me.txtpay.Name = "txtpay"
+        Me.txtpay.Size = New System.Drawing.Size(100, 20)
+        Me.txtpay.TabIndex = 1
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(232, 180)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 6
+        '
+        'txtresult
+        '
+        Me.txtresult.Location = New System.Drawing.Point(232, 180)
+        Me.txtresult.Name = "txtresult"
+        Me.txtresult.Size = New System.Drawing.Size(100, 20)
+        Me.txtresult.TabIndex = 2
+        '
+        'btcal
+        '
+        Me.btcal.Location = New System.Drawing.Point(173, 250)
+        Me.btcal.Name = "btcal"
+        Me.btcal.Size = New System.Drawing.Size(75, 23)
+        Me.btcal.TabIndex = 3
+        Me.btcal.Text = "คำนวณ"
+        Me.btcal.UseVisualStyleBackColor = True
+        '
+        'btclose
+        '
+        Me.btclose.Location = New System.Drawing.Point(282, 250)
+        Me.btclose.Name = "btclose"
+        Me.btclose.Size = New System.Drawing.Size(75, 23)
+        Me.btclose.TabIndex = 4
+        Me.btclose.Text = "ปิด"
+        Me.btclose.UseVisualStyleBackColor = True
+        '
+        'firstVB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(587, 493)
+        Me.Controls.Add(Me.btclose)
+        Me.Controls.Add(Me.btcal)
+        Me.Controls.Add(Me.txtresult)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.txtpay)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.txthour)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "firstVB"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtpay As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents txthour As System.Windows.Forms.TextBox
+    Friend WithEvents txtresult As System.Windows.Forms.TextBox
+    Friend WithEvents btcal As System.Windows.Forms.Button
+    Friend WithEvents btclose As System.Windows.Forms.Button
+
+End Class
---
+++ startVB/startVB/firstVB.vb
@@ -0,0 +1,11 @@
+﻿Public Class firstVB
+
+    Private Sub btcal_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btcal.Click
+        txtresult.Text = Val(txthour.Text) * Val(txtpay.Text)
+
+    End Sub
+
+    Private Sub btclose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btclose.Click
+        Me.Close()
+    End Sub
+End Class
